84a376d216b3366916b70adbc5f6ade55122051b
adrii0113 <adrian190401@gmail.com>
2023-07-26 23:35:37 +0200

    first commit

---
+++ src/webparts/listanoticias/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IListanoticiasWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'ListanoticiasWebPartStrings' {
+  const strings: IListanoticiasWebPartStrings;
+  export = strings;
+}
